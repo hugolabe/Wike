ce357f5483ed5490c9124aafe2e4905310389c46
Hari Rana <theevilskeleton@riseup.net>
2025-03-11 09:56:32 -0400

    Always set search mode to False
    If it's false, it will remain false.

--- src/page.py
+++ src/page.py
@@ -93,8 +93,7 @@ class PageBox(Gtk.Box):
     match event:
       case WebKit.LoadEvent.STARTED:
         self._is_main = wikiview.get_uri().endswith('.wikipedia.org/')
-        if self.search_bar.get_search_mode():
-          self.search_bar.set_search_mode(False)
+        self.search_bar.set_search_mode(False)
         self.view_stack.set_visible_child_name('wikiview')
         tabpage.set_title(_('Loading…'))
         tabpage.set_loading(True)
